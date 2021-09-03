eb7fc782bce2b65e44bd645cd7ca14e983dc3353
ashok0425 <ashokmehta123y@gmail.com>
2021-09-03 09:02:07 +0545

    Funconality to send email both to client and admin after user contact to admin and admin able to rply the user from mail using admin panel

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -2,51 +2,156 @@
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Contact;
 use App\Models\Website;
+use App\Models\Contact;
+use App\Models\Subscriber;
+use App\Models\Contactvendor;
 
-class ContactController extends Controller
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Mail\newslater;
+use App\Mail\contactmail;
+
+use Illuminate\Support\Facades\Mail;
+class ContactController  extends Controller
+{
+  public function index(){
+    $contact=Website::find(1);
+   return view('frontend.contact',compact('contact'));
+}
+
+public function store(Request $request){
+    $request->validate([
+
+        'email'=>'required|email',
+        'phone'=>'required',
+        'msg'=>'required',
+
+    ]);
+    try {
+
+
+    $cont=new Contact;
+    $cont->name=$request->name;
+    $cont->email=$request->email;
+    $cont->phone=$request->phone;
+    $cont->address=$request->address;
+
+    $cont->msg=$request->msg;
+    $cont->save();
+    $notification=array(
+        'alert-type'=>'success',
+        'messege'=>'Your query hasbeen recived.We will get back to you as soon as possible.',
+
+     );
+     return redirect()->back()->with($notification);
+
+     $data=array(
+        'msg'=>$request->msg,
+        'name'=>$request->name,
+        'address'=>$request->address,
+        'email'=>$request->email,
+        'phone'=>$request->phone,
+        'address'=>$request->address,
+
+
+
+
+
+    );
+    // Mail::to(Auth::user()->email)->send(new contactmail($data));
+
+
+} catch (\Throwable $th) {
+    //throw $th;
+    $notification=array(
+        'alert-type'=>'error',
+        'messege'=>'Something went wrong. Please try again later.',
+
+     );
+     return redirect()->back()->with($notification);
+}
+
+}
+
+
+public function subscribe(Request $request)
 {
 
-    public function index(){
-        $contact=Website::find(1);
-       return view('frontend.contact',compact('contact'));
+    $email=Subscriber::where('email',$request->email)->first();
+    if(!$email){
+         $request->validate([
+        'email'=>'required|unique:subscribers',
+    ]);
+    $newslater=new Subscriber;
+    $newslater->email=$request->email;
+    $newslater->save();
+        $notification=array(
+            'messege'=>'Thankyou for subscribing our Newsletter',
+            'alert-type'=>'success'
+             );
+             Mail::to($request->email)->send(new newslater);
+    }else{
+        $notification=array(
+            'messege'=>'Already Subscribed from this email !',
+            'alert-type'=>'error'
+             );
     }
 
-    public function store(Request $request){
-        $request->validate([
-            'name'=>'required',
-            'email'=>'required|email',
-            'phone'=>'required',
-            'message'=>'required',
+       return Redirect()->back()->with($notification);
+}
 
-        ]);
-        try {
-           
-       
-        $cont=new Contact;
-        $cont->fname=$request->name;
-        $cont->email=$request->email;
-        $cont->phone=$request->phone;
-        $cont->msg=$request->message;
+
+
+
+
+public function contactvendorstore(Request $request){
+    $request->validate([
+
+        'title'=>'required',
+        'message'=>'required',
+
+    ]);
+    try {
+       if(Auth::check()){
+        $cont=new Contactvendor;
+        $cont->user_id=Auth::user()->id;
+        $cont->title=$request->title;
+        $cont->message=$request->message;
+        $cont->vendor_id=$request->vendor_id;
         $cont->save();
         $notification=array(
             'alert-type'=>'success',
             'messege'=>'Your query hasbeen recived.We will get back to you as soon as possible.',
-            
+
          );
          return redirect()->back()->with($notification);
-
-    } catch (\Throwable $th) {
-        //throw $th;
+       }else{
         $notification=array(
-            'alert-type'=>'error',
-            'messege'=>'Something went wrong.Please try again later',
-            
-         );
-         return redirect()->back()->with($notification);
-    }
+            'alert-type'=>'info',
+            'messege'=>'Please Login.',
 
-    }
+         );
+         return redirect()->route('login')->with($notification);
+
+       }
+
+
+
+} catch (\Throwable $th) {
+    //throw $th;
+    $notification=array(
+        'alert-type'=>'error',
+        'messege'=>'Something went wrong. Please try again later.',
+
+     );
+     return redirect()->back()->with($notification);
+}
 
 }
+
+
+
+}
+
