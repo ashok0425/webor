0eac589f912c7e88e6c0610d7e26a759db02dc70
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2023-02-28 08:02:07 +0545

    remove our news title

--- resources/views/frontend/template/blog.blade.php
+++ resources/views/frontend/template/blog.blade.php
@@ -6,7 +6,7 @@
         ->get();
 @endphp
 <div class='my-container mt-20'>
-    <h3 class='sec-h1 text-4xl'>Our News</h3>
+    {{-- <h3 class='sec-h1 text-4xl'>Our News</h3> --}}
     <div class='grid sm:grid-cols-1 md:grid-cols-2 gap-11 mt-10'>
 
         @foreach ($blogs as $blog)
